db22b541ba536d59bec7f9a7072ea120330e617b
DannielF <danielgranados1992@gmail.com>
2022-02-26 09:38:15 -0500

    [Upd] use .env variables to connect db

--- dice-game-sk/db/mongo.connect.js
+++ dice-game-sk/db/mongo.connect.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
+const { config } = require('../config/index');
+
+const HOST = config.mongoHost;
+const PORT = config.mongoPort;
+const DB = config.mongoDatabase;
 
 const mongoConnect = async () => {
   try {
-    await mongoose.connect(`mongodb://localhost/test`);
+    await mongoose.connect(`mongodb://${HOST}:${PORT}/${DB}`);
     console.log('Connected to mongodb');
   } catch (err) {
-    console.log('Error connecting to mongodb', err);
+    console.error('Error connecting to mongodb', err);
   }
 };
 
 module.exports = { mongoConnect };
-
-// await mongoose.connect(`mongodb://${USER}:${PASSWORD}@${HOST}:${PORT}/test`);
